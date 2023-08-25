12b76f129e008fed97b31a6a3038c658453f1912
raziullah121 <raziullah.icp@gmail.com>
2023-08-25 12:42:19 +0500

    This is my initial commit

---
+++ js/index.js
@@ -0,0 +1,71 @@
+// $(document).ready(function(){
+//     alert('pakistan');
+// })
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     responsiveClass:true,
+//     responsive:{
+//         0:{
+//             items:1,
+//             nav:true
+//         },
+//         600:{
+//             items:3,
+//             nav:false
+//         },
+//         1000:{
+//             items:5,
+//             nav:true,
+//             loop:false
+//         }
+//     }
+// });
+
+$('.count').counterUp({
+    delay: 10,
+    time: 5000
+});
+
+$('.care').counterUp({
+    delay : 10,
+    time: 2000
+})
+
+
+//   document.addEventListener("DOMContentLoaded", function () {
+//     const images = document.querySelectorAll(".image-container");
+
+//     images.forEach(function (image) {
+//       image.addEventListener("click", function () {
+//         const img = image.querySelector("img");
+//         const imgSrc = img.getAttribute("src");
+//         window.open(imgSrc);
+//       });
+//     });
+//   });
+
+
+
+//   document.addEventListener("DOMContentLoaded", function () {
+//     const imageContainers = document.querySelectorAll(".image-container");
+
+//     imageContainers.forEach(function (container) {
+//       const image = container.querySelector("img");
+//       const icons = container.querySelector(".icons");
+//       const closeIcon = icons.querySelector(".close-icon");
+
+//       image.addEventListener("click", function () {
+//         container.classList.toggle("active");
+//       });
+
+//       closeIcon.addEventListener("click", function () {
+//         container.classList.remove("active");
+//       });
+//     });
+//   });
+
+
+
+
+
